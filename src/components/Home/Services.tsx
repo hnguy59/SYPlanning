38089f4d3e7a7d32c9a591ee745a7f404ce3fd8c
Henry Nguyen <riripishu@gmail.com>
2025-07-17 17:19:01 +1000

    Update wording for services

--- src/components/Home/Services.tsx
+++ src/components/Home/Services.tsx
@@ -221,6 +221,102 @@ export default function Services() {
               </ul>
             </div>
           </div>
+          {/* Pricing Card */}
+          <div className="flex flex-col lg:flex-row-reverse mx-auto text-neutral-900 bg-neutral-200 rounded-lg">
+            <Image
+              className="rounded-t-lg lg:rounded-r-lg lg:rounded-l-none w-full lg:w-1/3 grayscale object-cover object-left "
+              src="/images/generic/generic-house.jpg"
+              alt={''}
+              width={500}
+              height={500}
+            />
+            <div className="w-full lg:w-2/3 p-8 content-center">
+              <h3 className="mb-4 text-2xl font-semibold">Activation Precinct Certificates</h3>
+              {/* List */}
+              <ul className="space-y-4 text-left">
+                <li className="flex space-x-3">
+                  <span className="w-full">
+                    Since July 2018, the four Special Activation Precincts (SAP) have been declared
+                    by the NSW State Government. These SAPs are located within the Moree, Snowy
+                    Monaro, Parkes & Wagga Wagga local government areas. Unlike traditional approval
+                    pathways, developments within the SAP requires an Activation Precinct
+                    Certificate (APC) issued by the Regional Growth NSW Development Corporation
+                    (RGDC) pursuant to the State Environmental Planning Policy (Precincts—Regional)
+                    2021. Having previously worked within the planning team at RGDC, I am familiar
+                    with the APC process and will be able to assist in the following manner:
+                  </span>
+                </li>
+                <li className="flex space-x-3">
+                  {/* Icon */}
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>
+                    Planning lead for your proposed development, liaising with RGDC on your behalf
+                  </span>
+                </li>
+                <li className="flex space-x-3">
+                  {/* Icon */}
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>
+                    Preparation of a Statement of Consistency as required by the APC process
+                  </span>
+                </li>
+                <li className="flex space-x-3">
+                  {/* Icon */}
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>Lodgement of the APC application via the NSW Planning Portal</span>
+                </li>
+                <li className="flex space-x-3">
+                  {/* Icon */}
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>General project coordination</span>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </section>
